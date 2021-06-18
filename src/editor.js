9f01b0b6efd382549004786a416d0086a4fe118a
codewizard26 <nikhilmishra2608@gmail.com>
2021-06-18 11:41:41 +0530

    Second commit

--- src/editor.js
+++ src/editor.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import {EditorState} from 'draft-js';
+import {EditorState,convertToRaw, ContentState} from 'draft-js';
 import './App.css';
 import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from 'draft-convert';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { jsPDF } from "jspdf";
+
  class Textedit extends React.Component{
-     state={
-         editorState: EditorState.createEmpty(),
+     constructor(props){
+         super(props)
+         this.state={
+            editorState: EditorState.createEmpty(),
+            
+        }
      }
+     
      onEditorStateChange=(editorState)=>{
-         this.setState({editorState});
+       this.setState({editorState})
      }
+     onhandleSubmit=()=>{
+         const ContentState =  convertToHTML(this.state.editorState.getCurrentContent());
+         const formated=this.state.editorState.getCurrentInlineStyle()
+         console.log(ContentState)
+         console.log(formated);
+         let doc = new jsPDF("landscape", 'pt', 'A4');
+         doc.html(ContentState, {
+           callback: () => {
+            
+           }
+     });
+        
+        //  const blocks = convertToRaw(this.state.editorState.getCurrentContent()).blocks;
+        //  const value = blocks.map(block => (!block.text.trim() && '\n') || block.text).join('\n');
+    }
      render(){
          const {editorState}=this.state;
          return(
@@ -20,7 +43,9 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
              editorClassName="editorClassName"
              onEditorStateChange={this.onEditorStateChange}
          />
-         <button>Submit</button></div>);
+         <button style={{cursor:'pointer'}} onClick={this.onhandleSubmit}>Submit</button>
+         <div>{ContentState}</div>
+         </div>);
      }
  }
  export default Textedit;
